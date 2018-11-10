4f76abfd7db41d5a2e2d954e9e1fa02f17bc7259
nickito <nickito@192.168.0.17>
2018-11-10 16:29:20 -0300

    Limpieza de codigo primera parte

--- src/yog/Interfaz.java
+++ src/yog/Interfaz.java
@@ -8,6 +8,7 @@ import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
+import java.util.ArrayList;
 import java.util.Timer;
 import java.util.TimerTask;
 import java.util.regex.Matcher;
@@ -83,32 +84,11 @@ public class Interfaz {
 	
 	//Logica del solver
 	Solver solver=null;
+	ArrayList<String>nacionalidades=new ArrayList<String>();
+	ArrayList<String>deportes=new ArrayList<String>();
+	ArrayList<String> generos=new ArrayList<String>();
+	ArrayList <String> sugerencias=new ArrayList <String>();
 	
-	String[] paises={"Femenino","Masculino","Atletismo","Badminton","Baloncesto3x3",
-"Balonmano de playa","Boxeo","Breaking","Ciclismo","Ecuestre","Escalada deportiva","Esgrima","Futsal",
-"Gimnasia acrobatica","Gimnasia artistica","Gimnasia ritmica","Gimnasia Trampolin","Gimnasia trampolin","Golf","Hockey 5",
-"Judo","Karate","Levantamientodepesas","Lucha","Natacion","Patinaje de velocidad sobre ruedas",
-"Pentatlonmoderno","Piraguismo","Remo","Rugby siete","Saltos","Taekwondo","Tenis","Tenis de mesa","Tiro con arco",
-"Tirodeportivo","Triatlon","Vela","Voley-playa","Afganistan","Albania","Alemania","Andorra","Angola","Antigua y Barbuda",
-"Arabia Saudi","Argelia","Argentina","Armenia","Aruba","Australia","Austria","Azerbaiyan","Bahamas","Bahrein","Bangladesh",
-"Barbados","Belgica","Belice","Benin","Bermudas","Bhutan","Bielorrusia","Bolivia","Bosnia","Botsuana","Brasil","Brunei","Bulgaria","Burkina Faso",
-"Burundi","Cabo Verde","Camboya","Camerun","Canada","Chad","Chile","China Taipei","Chipre","Colombia","Comoras","Congo","Costa de marfil",
-"CostaRica","Croacia","Cuba","Dinamarca","Djibouti","Dominica","Ecuador","Egipto","El salvador","Emiratos arabes unidos", 
-"Eritrea","Eslovaquia","Eslovenia","Espania","Estados Federados de micronesia","Estados Unidos","Estonia","Eswatini",
-"Etiopia","Ex republica Yugoslava","Federacion Rusa","Filipinas","Finlandia","Fiyi","Francia","Gabon","Gambia","Georgia","Ghana",
-"Gran Bretana","Granada","Grecia","Guam","Guatemala","Guinea","Guineaecuatorial","Guinea-Bissau","Guyana","Haiti","Honduras",
-"Hong Kong","Hungria","India","Indonesia","Iraq","Irlanda","Islandia","Islas Caiman","Islas Cook","Islas Marshall","Islas Salomon","Islas Virgenes Britanicas",
-"Islas Virgenes Estado Unidenses","Israel","Italia","Jamaica","Japon","Jordania","Kazajstan","Kenia","Kirguistan","Kiribati","Kosovo",
-"Kuwait","Lesotho","Letonia","Libano","Liberia","Libia","Liechtenstein","Lituania","Luxemburgo","Madagascar","Malasia","Malawi","Maldivas","Mali",
-"Malta","Marruecos","Mauricio","Mauritania","Mexico","Monaco","Mongolia","Montenegro","Mozambique","Myanmar","Namibia","Nauru","Nepal","Nicaragua","Niger","Nigeria",
-"Noruega","Nueva Zelanda","Oman","PaisesBajos","Pakistan","Palaos","Palestina","Panama","Papua-Nueva Guinea","Paraguay","Peru","Polonia","Portugal","PuertoRico","Qatar","Republica Arabe Siria","Republica Centroafricana",
-"Republica Checa","Republica de Corea","Republica de Moldavia","Republica de Timor Oriental","Republica del Congo","Republica de Lao","Republica Dominicana",
-"Republica de Iran","Republica de China","Republica democratica de Corea","Republica de Tanzania","Ruanda","Rumania","Samoa","Samoa Americana","San Cristobal Y nieves","San Marino","San Vicentey las Granadinas",
-"Santa Lucia","Santo Tome y principe","Senegal","Serbia","Seychelles","Sierra Leona","Singapur","Somalia","Sri Lanka","Sudafrica",
-"Sudan","SudandelSur","Suecia","Suiza","Surinam","Tailandia","Tayikistan","Togo","Tonga","Trinidad y Tobago","Tunez","Turkmenistan","Turquia","Tuvalu",
-"Ucrania","Uganda","Uruguay","Uzbekistan","Vanuatu","Venezuela","Vietnam","Yemen","Zambia","Zimbabue",};
-	
-
 	/**
 	 * Launch the application.
 	 */
@@ -133,6 +113,58 @@ public class Interfaz {
 	public Interfaz() 
 	{
 		initialize();
+	}
+	
+	public void volcarValores(Atleta[] atletas)
+	{
+		for (Atleta a : atletas) 
+		{
+			String nacionalidad=a.getNacionality();
+			String sexo=a.getGenre();
+			String deporte=a.getSport();
+			
+			if (!nacionalidades.contains(nacionalidad) )
+			{
+				nacionalidades.add(nacionalidad);
+			}
+			
+			if (!deportes.contains(deporte))
+			{
+				deportes.add(deporte);
+			}
+			
+			if (generos.contains(sexo)) 
+			{
+				generos.add(sexo);
+			}
+			
+		}
+	}
+	
+	private void llenarComboBox(JComboBox<String> combo) 
+	{
+		combo.addItem("Seleccionar...");
+		combo.addItem("[Genero]");
+		for (String sex : generos ) 
+		{
+			combo.addItem(sex);
+			sugerencias.add(sex);
+		}
+		combo.addItem("[Pais]");
+		for(String pa: nacionalidades) 
+		{
+			combo.addItem(pa);
+			sugerencias.add(pa);
+		}
+		combo.addItem("[Deporte]");
+		for (String dep: deportes)
+		{
+			combo.addItem(dep);
+			sugerencias.add(dep);
+		}
+		
+		
+		
 	}
 
 	public String capitalizar(String str) 
@@ -159,6 +191,8 @@ public class Interfaz {
 		}
 		return false;
 	}
+	
+	
 	
 	private void filtro(String consulta, JTable jtableBuscar)
 	{
@@ -613,11 +647,9 @@ public class Interfaz {
 		imagen.setToolTipText("Ir a la pagina oficial de las olimpiadas");
 		frame.add(imagen);
 		
-		TextAutoCompleter textAutoCompleter = new TextAutoCompleter(text);
-		for (String e: paises) 
-		{
-			textAutoCompleter.addItem(e);
-		}
+		
+		
+		
 		
 		
 		tarea2=new TimerTask()
@@ -795,255 +827,7 @@ public class Interfaz {
 		JComboBox<String> combo=new JComboBox<String>();
 		combo.setVisible(false);
 		
-		combo.addItem("Seleccionar...");
-		combo.addItem("[Genero]");
-		combo.addItem("Femenino");
-		combo.addItem("Masculino");
-		combo.addItem("[Deporte]");
-		combo.addItem("Atletismo");
-		combo.addItem("Badminton");
-		combo.addItem("Baloncesto 3x3");
-		combo.addItem("Balonmano de playa");
-		combo.addItem("Boxeo");
-		combo.addItem("Breaking");
-		combo.addItem("Ciclismo");
-		combo.addItem("Ecuestre");
-		combo.addItem("Escalada deportiva");
-		combo.addItem("Esgrima");
-		combo.addItem("Futsal");
-		combo.addItem("Gimnasia acrobatica");
-		combo.addItem("Gimnasia artistica");
-		combo.addItem("Gimnasia ritmica");
-		combo.addItem("Gimnasia Trampolin");
-		combo.addItem("Gimnasia trampolin");
-		combo.addItem("Golf");
-		combo.addItem("Hockey 5");
-		combo.addItem("Judo");
-		combo.addItem("Karate");
-		combo.addItem("Levantamiento de pesas");
-		combo.addItem("Lucha");
-		combo.addItem("Natacion");
-		combo.addItem("Patinaje de velocidad sobre ruedas");
-		combo.addItem("Pentatlon moderno");
-		combo.addItem("Piraguismo");
-		combo.addItem("Remo");
-		combo.addItem("Rugby siete");
-		combo.addItem("Saltos");
-		combo.addItem("Taekwondo");
-		combo.addItem("Tenis");
-		combo.addItem("Tenis de mesa");
-		combo.addItem("Tiro con arco");
-		combo.addItem("Tiro deportivo");
-		combo.addItem("Triatlon");
-		combo.addItem("Vela");
-		combo.addItem("Voley-playa");
-		combo.addItem("[Pais]");
-		combo.addItem("Afganistan");
-		combo.addItem("Albania");
-		combo.addItem("Alemania");
-		combo.addItem("Andorra");
-		combo.addItem("Angola");
-		combo.addItem("Antigua y Barbuda");
-		combo.addItem("Arabia Saudi");
-		combo.addItem("Argelia");
-		combo.addItem("Argentina");
-		combo.addItem("Armenia");
-		combo.addItem("Aruba");
-		combo.addItem("Australia");
-		combo.addItem("Austria");
-		combo.addItem("Azerbaiyan");
-		combo.addItem("Bahamas");
-		combo.addItem("Bahrein");
-		combo.addItem("Bangladesh");
-		combo.addItem("Barbados");
-		combo.addItem("Belgica");
-		combo.addItem("Belice");
-		combo.addItem("Benin");
-		combo.addItem("Bermudas");
-		combo.addItem("Bhutan");
-		combo.addItem("Bielorrusia");
-		combo.addItem("Bolivia");
-		combo.addItem("Bosnia");
-		combo.addItem("Botsuana");
-		combo.addItem("Brasil");
-		combo.addItem("Brunei");
-		combo.addItem("Bulgaria");
-		combo.addItem("Burkina Faso");
-		combo.addItem("Burundi");
-		combo.addItem("Cabo Verde");
-		combo.addItem("Camboya");
-		combo.addItem("Camerun");
-		combo.addItem("Canada");
-		combo.addItem("Chad");
-		combo.addItem("Chile");
-		combo.addItem("China Taipei");
-		combo.addItem("Chipre");
-		combo.addItem("Colombia");
-		combo.addItem("Comoras");
-		combo.addItem("Congo");
-		combo.addItem("Costa de marfil");
-		combo.addItem("Costa Rica");
-		combo.addItem("Croacia");
-		combo.addItem("Cuba");
-		combo.addItem("Dinamarca");
-		combo.addItem("Djibouti");
-		combo.addItem("Dominica");
-		combo.addItem("Ecuador");
-		combo.addItem("Egipto");
-		combo.addItem("El salvador");
-		combo.addItem("Emiratos arabes unidos");
-		combo.addItem("Eritrea");
-		combo.addItem("Eslovaquia");
-		combo.addItem("Eslovenia");
-		combo.addItem("Espania");
-		combo.addItem("Estados Federados de micronesia");
-		combo.addItem("Estados Unidos");
-		combo.addItem("Estonia");
-		combo.addItem("Eswatini");
-		combo.addItem("Etiopia");
-		combo.addItem("Ex republica Yugoslava");
-		combo.addItem("Federacion Rusa");
-		combo.addItem("Filipinas");
-		combo.addItem("Finlandia");
-		combo.addItem("Fiyi");
-		combo.addItem("Francia");
-		combo.addItem("Gabon");
-		combo.addItem("Gambia");
-		combo.addItem("Georgia");
-		combo.addItem("Ghana");
-		combo.addItem("Gran Bretana");
-		combo.addItem("Granada");
-		combo.addItem("Grecia");
-		combo.addItem("Guam");
-		combo.addItem("Guatemala");
-		combo.addItem("Guinea");
-		combo.addItem("Guinea ecuatorial");
-		combo.addItem("Guinea-Bissau");
-		combo.addItem("Guyana");
-		combo.addItem("Haiti");
-		combo.addItem("Honduras");
-		combo.addItem("Hong Kong");
-		combo.addItem("Hungria");
-		combo.addItem("India");
-		combo.addItem("Indonesia");
-		combo.addItem("Iraq");
-		combo.addItem("Irlanda");
-		combo.addItem("Islandia");
-		combo.addItem("Islas Caiman");
-		combo.addItem("Islas Cook");
-		combo.addItem("Islas Marshall");
-		combo.addItem("Islas Salomon");
-		combo.addItem("Islas Virgenes Britanicas");
-		combo.addItem("Islas Virgenes EstadoUnidenses");
-		combo.addItem("Israel");
-		combo.addItem("Italia");
-		combo.addItem("Jamaica");
-		combo.addItem("Japon");
-		combo.addItem("Jordania");
-		combo.addItem("Kazajstan");
-		combo.addItem("Kenia");
-		combo.addItem("Kirguistan");
-		combo.addItem("Kiribati");
-		combo.addItem("Kosovo");
-		combo.addItem("Kuwait");
-		combo.addItem("Lesotho");
-		combo.addItem("Letonia");
-		combo.addItem("Libano");
-		combo.addItem("Liberia");
-		combo.addItem("Libia");
-		combo.addItem("Liechtenstein");
-		combo.addItem("Lituania");
-		combo.addItem("Luxemburgo");
-		combo.addItem("Madagascar");
-		combo.addItem("Malasia");
-		combo.addItem("Malawi");
-		combo.addItem("Maldivas");
-		combo.addItem("Mali");
-		combo.addItem("Malta");
-		combo.addItem("Marruecos");
-		combo.addItem("Mauricio");
-		combo.addItem("Mauritania");
-		combo.addItem("Mexico");
-		combo.addItem("Monaco");
-		combo.addItem("Mongolia");
-		combo.addItem("Montenegro");
-		combo.addItem("Mozambique");
-		combo.addItem("Myanmar");
-		combo.addItem("Namibia");
-		combo.addItem("Nauru");
-		combo.addItem("Nepal");
-		combo.addItem("Nicaragua");
-		combo.addItem("Niger");
-		combo.addItem("Nigeria");
-		combo.addItem("Noruega");
-		combo.addItem("Nueva Zelanda");
-		combo.addItem("Oman");
-		combo.addItem("Paises Bajos");
-		combo.addItem("Pakistan");
-		combo.addItem("Palaos");
-		combo.addItem("Palestina");
-		combo.addItem("Panama");
-		combo.addItem("Papua-Nueva Guinea");
-		combo.addItem("Paraguay");
-		combo.addItem("Peru");
-		combo.addItem("Polonia");
-		combo.addItem("Portugal");
-		combo.addItem("Puerto Rico");
-		combo.addItem("Qatar");
-		combo.addItem("Republica Arabe Siria");
-		combo.addItem("Republica Centroafricana");
-		combo.addItem("Republica Checa");
-		combo.addItem("Republica de Corea");
-		combo.addItem("Republica de Moldavia");
-		combo.addItem("Republica de Timor Oriental");
-		combo.addItem("Republica del Congo");
-		combo.addItem("Republica de Lao");
-		combo.addItem("Republica Dominicana");
-		combo.addItem("Republica de Iran");
-		combo.addItem("Republica de China");
-		combo.addItem("Republica democratica de Corea");
-		combo.addItem("Republica de Tanzania");
-		combo.addItem("Ruanda");
-		combo.addItem("Rumania");
-		combo.addItem("Samoa");
-		combo.addItem("Samoa Americana");
-		combo.addItem("San Cristobal Y nieves");
-		combo.addItem("San Marino");
-		combo.addItem("San Vicente y las Granadinas");
-		combo.addItem("Santa Lucia");
-		combo.addItem("Santo Tome y principe");
-		combo.addItem("Senegal");
-		combo.addItem("Serbia");
-		combo.addItem("Seychelles");
-		combo.addItem("Sierra Leona");
-		combo.addItem("Singapur");
-		combo.addItem("Somalia");
-		combo.addItem("Sri Lanka");
-		combo.addItem("Sudafrica");
-		combo.addItem("Sudan");
-		combo.addItem("Sudan del Sur");
-		combo.addItem("Suecia");
-		combo.addItem("Suiza");
-		combo.addItem("Surinam");
-		combo.addItem("Tailandia");
-		combo.addItem("Tayikistan");
-		combo.addItem("Togo");
-		combo.addItem("Tonga");
-		combo.addItem("Trinidad y Tobago");
-		combo.addItem("Tunez");
-		combo.addItem("Turkmenistan");
-		combo.addItem("Turquia");
-		combo.addItem("Tuvalu");
-		combo.addItem("Ucrania");
-		combo.addItem("Uganda");
-		combo.addItem("Uruguay");
-		combo.addItem("Uzbekistan");
-		combo.addItem("Vanuatu");
-		combo.addItem("Venezuela");
-		combo.addItem("Vietnam");
-		combo.addItem("Yemen");
-		combo.addItem("Zambia");
-		combo.addItem("Zimbabue");
+
 		
 		
 		combo.setBounds(780,25,120,50);
@@ -1505,6 +1289,7 @@ public class Interfaz {
 					{
 						 atletas= gson.fromJson(json, Atleta[].class );
 						 
+						 
 					}
 					catch (Exception r) 
 					{
@@ -1518,6 +1303,14 @@ public class Interfaz {
 							
 							try 
 						{
+							volcarValores(atletas);
+							llenarComboBox(combo);
+							TextAutoCompleter textAutoCompleter = new TextAutoCompleter(text);
+							for (String texto: sugerencias) 
+							{
+								textAutoCompleter.addItem(texto);
+							}
+							
 							objetos=new Object[atletas.length][5];
 							
 						}
@@ -1583,6 +1376,7 @@ public class Interfaz {
 						}
 					
 			}
+		
 			
 		});
 		
@@ -1672,17 +1466,11 @@ public class Interfaz {
 							{
 								if (continuar)
 								{
-									JOptionPane.showMessageDialog(selector, "Se copio el modelo de excel en "+destinoPath.toString());
-									
-									
+									JOptionPane.showMessageDialog(selector, "Se copio el modelo de excel en "+destinoPath.toString());	
 								}
 								
-								
 							}
-					        
-					        
-					    } 
-					    
+					   }   
 				  }
 					 
 					 fc.setFileSelectionMode(JFileChooser.FILES_ONLY);
